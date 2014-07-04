1ace26f659b7d1daf9863265f802b8e85c4a52ef
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-07-03 17:03:17 -0700

    Added stories to database and created story controller and view

--- Tweetlibs/app/controllers/story_controller.rb
+++ Tweetlibs/app/controllers/story_controller.rb
@@ -1,5 +1,7 @@
 class StoryController < ApplicationController
   def show
-
+    @chosen_story = Story.find(1)
+    @first_tweets = params[:user_name1]
+    @second_tweets = params[:user_name2]
   end
 end
