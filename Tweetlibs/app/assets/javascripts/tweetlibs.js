214048a0fb2b1bbc15c09b4e7eb8e65c93a43b61
cdelauder <cdelauder@gmail.com>
2014-07-22 09:18:03 -0700

    figured out the problem. I was being dumb and removing the target of the event delegation. Along the way i did learn an interesting tidbit in that you can bind multiple listeners in the same line of code. we can now display the loading tweets... while we wait for the ajax call to complete

--- Tweetlibs/app/assets/javascripts/tweetlibs.js
+++ Tweetlibs/app/assets/javascripts/tweetlibs.js
@@ -1,21 +1,34 @@
 $(document).ready(bindListeners)
 
 function bindListeners() {
-  $('.container').one('ajax:success', '#new_story', askForTwitternames)
-  $('.container').one('ajax:before', '#new_tweet', loading)
-  $('.container').one('ajax:success', '#new_tweet', showTweets)
+  $('.container').on('ajax:success', '#new_story', askForTwitternames)
+  $('.container').on('ajax:success ajax:send', '#new_tweet', tweetHelper)
+  // $('.container').on('ajax:send', '#new_tweet', loading)
 }
 
 function askForTwitternames(e, data, status, xhr) {
   $('.bottom').append(xhr.responseText)
+  // $('.container').off('ajax:success', '#new_story', askForTwitternames)
 }
 
-function loading() {
-  $('.bottom').html('Loading tweets...')
+function tweetHelper(e, data, status, xhr) {
+  debugger
+  if (e.type === 'ajax:send') {
+    loading()
+  } else {
+    showTweets(e, data, status, xhr)
+  }
+}
+
+function loading(e, data, status, xhr) {
+  $('.bottom').append('Loading tweets...')
 }
 
 function showTweets(e, data, status, xhr) {
+  console.log('balls')
   $('.container').empty()
   $('.container').append(xhr.responseText)
+  $('.container').off('ajax:send', '#new_tweet', loading)
+  $('.container').off('ajax:success', '#new_tweet', showTweets)
 }
 
