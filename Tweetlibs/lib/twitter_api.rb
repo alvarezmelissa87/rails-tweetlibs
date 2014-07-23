3c5b1f3723323a4dc03d877498dc0ae16db87f13
cdelauder <cdelauder@gmail.com>
2014-07-23 15:32:57 -0700

    utilized a different module pattern to refactor the api module. By including a module within I was able to add methods available with the api module

--- Tweetlibs/lib/twitter_api.rb
+++ Tweetlibs/lib/twitter_api.rb
@@ -1,27 +1,53 @@
 module TwitterApi
 
-  def get_tweets(twittername)
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token        = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-    end
+  def self.included(base)
+    base.include Twitter_helper
+  end
 
+  def get_tweets(twittername)
+    client = self.make_client
     strings = []
     tweets = client.user_timeline(twittername)
+    self.process_tweets(tweets, strings)
+  end
 
-    tweets.sample(9).each do |tweet|
-      remove_urls_hagtags = /(https?:\/\/)?([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})([\/\w\.-]*)*\/?|@\w+|#\w+/
-      remove_punctuations = /(\.|!|:|--|-)?$/
-      remove_retweets = /(RT\s:)/
-      urls_removed = tweet.text.gsub(remove_urls_hagtags, "").strip
-      punc_removed = urls_removed.gsub(remove_punctuations, "").strip
-      rts_removed = punc_removed.gsub(remove_retweets, "").strip
-      strings << rts_removed if rts_removed.length > 2
+  module Twitter_helper
+    def make_client
+      p 'client'
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token        = ENV['ACCESS_TOKEN']
+        config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+      end
     end
 
-    strings
+    def process_tweets(tweets, strings)
+      tweets.sample(9).each {|tweet| format_tweet(tweet, strings)}
+      return strings
+    end
+
+    def format_tweet(tweet, strings)
+      tweet = remove_url(tweet)
+      tweet = remove_punctuation(tweet)
+      strings << remove_retweet(tweet)
+    end
+
+    def remove_url(tweet)
+      remove_urls_hagtags = /(https?:\/\/)?([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})([\/\w\.-]*)*\/?|@\w+|#\w+/
+      tweet.text.gsub(remove_urls_hagtags, "").strip
+    end
+
+    def remove_punctuation(tweet)
+      remove_punctuations = /(\.|!|:|--|-)?$/
+      tweet.gsub(remove_punctuations, "").strip
+    end
+
+    def remove_retweet(tweet)
+      remove_rts = /(RT\s:)/
+      tweet.gsub(remove_rts, "").strip
+    end
+
   end
 end
 
